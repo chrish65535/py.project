775344f7c8f05c6a5d215475c9a6fb453b371a64
chrish65535 <chrish216_dev@163.com>
2021-03-01 16:52:35 +0800

    init

---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='py.project',
+    version='1.0.0',
+    description='a template py.project',
+    long_description=readme,
+    author='chrish65535',
+    author_email='chrish216_dev@163.com',
+    url='https://github.com/chrish65535/py.project',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
+
